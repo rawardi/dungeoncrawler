43164c5a75c68d546fec16cb5e9d0c70de5a642e
rawardi <160549735+rawardi@users.noreply.github.com>
2025-01-10 11:03:24 +0100

    room movement fixed but not totally fixed

--- dungeoncrawling/commandprocessor.gd
+++ dungeoncrawling/commandprocessor.gd
@@ -3,7 +3,15 @@ extends Node
 
 var start=false
 var travel=false
-
+@onready var room1=$roomhandler/room1
+@onready var room2=$roomhandler/room2
+@onready var room3=$roomhandler/room3
+var can_go_right = false
+@onready var currentroomright=$roomhandler/room1
+var targetroomright :Node2D 
+var can_go_left=false
+@onready var currentroomleft=$roomhandler/room1
+var targetroomleft :Node2D 
 func  process(input:String) :
 	if start == false :
 		match input :
@@ -12,17 +20,45 @@ func  process(input:String) :
 				return "please enjoy the game"
 			_:
 				return "please start the game"
+	if travel :
+		match input :
+			"right" :
+				if can_go_right:
+					travel=false
+					$roomhandler/Sprite2D.global_position=targetroomright.global_position
+					currentroomright=targetroomright
+					can_go_right=false
+					return "You go right."
+				else:
+					travel=false
+					return "You cannot go right."
+			"left" :
+				if can_go_left:
+					travel=false
+					$roomhandler/Sprite2D.global_position=targetroomleft.global_position
+					currentroomleft=targetroomleft
+					can_go_left=false
+					return "You go left."
+				else:
+					travel=false
+					return "You cannot go left."
+			_:
+				travel=false
+				return "doesnt recognized direction"
 	else :
 		match input :
+			"start" :
+				return "you already start the game"
 			"help" :
 				return help(input)
 			"go" :
-				return "go where"
 				travel=true
+				return "go where"
 			"pick" :
 				return pickitem(input)
 			"inventory" :
 				return checkinventory(input)
+
 
 
 
@@ -33,11 +69,23 @@ func help(input:String) :
 			pick : pick up stuff up
 			inventory ;to check inventory"
 
-func go(input:String) :
-	return "this go function worked"
-
 func pickitem(input:String) :
 	return "pick fucntion worked"
 
 func checkinventory(input:String) :
 	return "check Inventory"
+
+
+func _on_right_area_entered(area: Area2D) -> void:
+	targetroomright=area
+	can_go_right=true
+
+
+func _on_left_area_entered(area: Area2D) -> void:
+	targetroomleft=area
+	can_go_left=true
+
+
+func _on_right_body_entered(body: Node2D) -> void:
+	targetroomright=body
+	can_go_right=true
